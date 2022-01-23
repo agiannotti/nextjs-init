7ab2adb2aa33e6d98db36c469e68c56070cddfaf
Anthony Giannotti <aj.gianno@Anthonys-MacBook-Pro.local>
2022-01-23 14:16:55 -0600

    styling, structure, theme extends, container config;

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,16 @@
 import React from "react";
+import Sidebar from "components/Sidebar/Sidebar";
+import Dashboard from "components/Dashboard/Dashboard";
 
 const Home = () => {
-  return <div className="">Hello World</div>;
+  return (
+    <div className="container flex flex-col h-screen m-auto">
+      <div className="flex flex-1 my-2 mx-1 bg-white rounded-2xl shadow-2xl">
+        <Sidebar />
+        <Dashboard />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,16 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gray: "#eee",
+        silver: "#ecebff",
+        white: "#FFFFFF",
+        primary: "#1B263A",
+        secondary: "#F5FBFF",
+        accent: "#FC6767",
+      },
+    },
   },
   plugins: [],
 };
